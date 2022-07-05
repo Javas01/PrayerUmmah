64deaffc440bb263e481a270b77c640e3e8dea44
Jawwaad Sabree <jawwaad.sabree@showseeker.com>
2022-07-05 17:54:28 -0400

    Initial Commit

---
+++ Shared/PrayerUmmahApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrayerUmmahApp.swift
+//  Shared
+//
+//  Created by Jawwaad Sabree on 7/5/22.
+//
+
+import SwiftUI
+
+@main
+struct PrayerUmmahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
